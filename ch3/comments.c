153ed3f0b7a27670104f1264868301cbe145eb46
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-09-22 22:28:15 +0200

    Files for the second recipe

---
+++ ch3/comments.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // A C99 comment
+    printf("hello, world\n");
+    return 0;
+}
---
+++ ch3/for-test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 10; i>0; i--)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
