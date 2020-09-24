0dd2c9d6350afe4a0a45c88e7df40d53c234d473
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-09-24 17:55:00 +0200

    Added sanity checks to make it more complete

--- ch3/is-it-a-prime.c
+++ ch3/is-it-a-prime.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "prime.h"
 
 int main(int argc, char *argv[])
 {
     long int num;
-    num = atol(argv[1]);
-    if (isprime(num))
+    /* Only one argument is accepted */
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s number\n", argv[0]);
+        return 1;
+    }
+    /* Only numbers 0-9 are accepted */
+    if ( strspn(argv[1], "0123456789") != strlen(argv[1]) )
+    {
+        fprintf(stderr, "Only numeric values are accepted\n");
+        return 1;
+    }
+    num = atol(argv[1]); /* String to long */
+    if (isprime(num)) /* Check if num is a prime */
     {
         printf("%ld is a prime\n", num);
     }
