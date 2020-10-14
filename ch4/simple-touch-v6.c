7e94e34475a04946f8552403e93eecbd66fa26c3
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-10-15 00:09:10 +0200

    Added more versions of 'simple-touch'

---
+++ ch4/simple-touch-v6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#define MAX_LENGTH 100
+
+int main(int argc, char *argv[])
+{
+    char filename[MAX_LENGTH] = { 0 };
+    if (argc != 2)
+    {
+        fprintf(stderr, "You must supply a filename as "
+            "an argument\n");
+        return 1;
+    }
+    strncat(filename, argv[1], sizeof(filename)-1);
+    if ( creat(filename, 00644) == -1 )
+    {
+        perror("Can't create file");
+        return 1;
+    }
+    return 0;
+}
