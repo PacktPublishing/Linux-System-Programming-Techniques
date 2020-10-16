e70e1c8e587132327d447733cae3ecf4a7ae8ab6
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-10-17 01:19:57 +0200

    Added source file for recipe one

---
+++ ch5/my-stat-v1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    struct stat filestat;
+    if ( argc != 2 )
+    {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    if ( stat(argv[1], &filestat) == -1 )
+    {
+        fprintf(stderr, "Can't read file %s: %s\n", 
+            argv[1], strerror(errno));
+        return errno;
+    }
+    printf("Inode: %lu\n", filestat.st_ino);
+    printf("Size: %ld\n", filestat.st_size);
+    printf("Links: %lu\n", filestat.st_nlink);
+    return 0;
+}
