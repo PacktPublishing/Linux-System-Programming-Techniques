e021dcde865dc4b1390d4931d2eedd40470c52e0
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-10-18 01:43:29 +0200

    Programs to create links

---
+++ ch5/new-name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s [target]" 
+            "[new-name]\n", argv[0]);
+        return 1;
+    }
+    if (link(argv[1], argv[2]) == -1)
+    {
+        perror("Can't create link");
+        return 1;
+    }
+    return 0;
+}
---
+++ ch5/new-symlink.c
@@ -0,0 +1,21 @@
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s [target]" 
+            "[link]\n", argv[0]);
+        return 1;
+    }
+    if (symlink(argv[1], argv[2]) == -1)
+    {
+        perror("Can't create link");
+        return 1;
+    }
+    return 0;
+}
