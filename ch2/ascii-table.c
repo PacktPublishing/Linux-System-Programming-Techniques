45d236530b3374085ce53dc4542e93e34f07d46d
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-11-13 20:46:32 +0100

    Added program to display an ASCII-table

---
+++ ch2/ascii-table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+    for (c = 65; c<=90; c++)
+    {
+        printf("%c = %d    ", c, c);
+        printf("%c = %d\n", c+32, c+32);
+    }
+    return 0;
+}
