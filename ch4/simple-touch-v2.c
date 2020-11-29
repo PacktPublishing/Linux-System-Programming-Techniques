5ede27cb7ac23dd114695cad236803e9222917f6
Jack-Benny Persson <jack-benny@cyberinfo.se>
2020-11-29 18:17:14 +0100

    Use PATH_MAX from linux/limits.h instead of our own macro

--- ch4/simple-touch-v2.c
+++ ch4/simple-touch-v2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
-#define MAX_LENGTH 100
+#include <linux/limits.h>
 
 int main(int argc, char *argv[])
 {
-    char filename[MAX_LENGTH] = { 0 };
+    char filename[PATH_MAX] = { 0 };
     if (argc != 2)
     {
         fprintf(stderr, "You must supply a filename as "
             "an argument\n");
         return 1;
     }
-    strncat(filename, argv[1], MAX_LENGTH-1);
+    strncpy(filename, argv[1], PATH_MAX-1);
     if ( creat(filename, 00644) == -1 )
     {
         fprintf(stderr, "Can't create file %s\n", filename);
