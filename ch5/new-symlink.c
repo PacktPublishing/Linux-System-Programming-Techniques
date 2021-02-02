c6eb4f61eda704d00717d59c48126a5c5e504391
Jack-Benny Persson <jack-benny@cyberinfo.se>
2021-02-02 18:12:02 +0100

    Forgotten a space

--- ch5/new-symlink.c
+++ ch5/new-symlink.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        fprintf(stderr, "Usage: %s [target]" 
+        fprintf(stderr, "Usage: %s [target] " 
             "[link]\n", argv[0]);
         return 1;
     }
